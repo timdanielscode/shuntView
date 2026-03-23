7c639ac41d0f8888642df0d462e4aa42e66b71bb
timdanielscode <timdanielscode@hotmail.com>
2026-03-23 14:26:01 +0000

    Update css file

--- app/views/trainer/index.php
+++ app/views/trainer/index.php
@@ -5,6 +5,7 @@
         <title>ShuntView</title>
         <meta name="viewport" content="width=device-width,initial-scale=1">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+        <link href="/assets/css/style.css" rel="stylesheet">
         <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
         <script defer src="/assets/js/Encounter.js" type="text/javascript"></script>
         <script defer src="/assets/js/main.js" type="text/javascript"></script>
@@ -59,21 +60,28 @@
 
                     </div>
                     <div class="col">
-                        <form method="POST" action="/trainer/<?php echo $id[0]; ?>">
-                            <button id="encounters" type="button" name="count" value="<?php echo $encounters['encounters']; ?>"><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/<?php echo $pokemonId; ?>.png"/></button>
-                            <input type="text" name="encounters" value="<?php echo $encounters; ?>"/>
-                            <input type="hidden" name="pokemonId" value="<?php echo $pokemonId; ?>"/>
-                            <input type="hidden" name="gameId" value="<?php echo $gameId; ?>"/>
-                            <input type="submit" name="submit" value="Save!"/>
-                        </form>
+
+                        <?php if(!empty($pokemon) === true) { ?>
+
+                            <form method="POST" action="/trainer/<?php echo $id[0]; ?>">
+                                <button id="encounters" type="button" name="count" value="<?php echo $encounters['encounters']; ?>"><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/<?php echo $pokemonId; ?>.png"/></button>
+                                <input type="text" name="encounters" value="<?php echo $encounters; ?>"/>
+                                <input type="hidden" name="pokemonId" value="<?php echo $pokemonId; ?>"/>
+                                <input type="hidden" name="gameId" value="<?php echo $gameId; ?>"/>
+                                <input type="submit" name="submit" value="Save!"/>
+                            </form>
+
+                        <?php } ?>
                     </div>
                     <div class="col">
-                        <div class="pokemonSprites">
-                            <?php for($i = 1; $i <= 807; $i++) { ?>
-                                <form method="GET" action="/trainer/<?php echo $id[0]; ?>/new">
-                                    <button type="submit" name="pokemonId" value="<?php echo $i; ?>"><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-vii/ultra-sun-ultra-moon/<?php echo $i; ?>.png"/></button>
-                                </form>
-                            <?php } ?>
+                        <div class="pokemonSpritesContainer">
+                            <div class="pokemonSprites">
+                                <?php for($i = 1; $i <= 807; $i++) { ?>
+                                    <form method="GET" action="/trainer/<?php echo $id[0]; ?>/new">
+                                        <button type="submit" name="pokemonId" value="<?php echo $i; ?>"><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-vii/ultra-sun-ultra-moon/<?php echo $i; ?>.png"/></button>
+                                    </form>
+                                <?php } ?>
+                            </div>
                         </div>
                     </div>
                 </div>
